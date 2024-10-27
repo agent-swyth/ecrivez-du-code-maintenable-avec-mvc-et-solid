2af897ec17641aecc5098c46b3da0d96193fc701
swyth_ <swyth+github@pm.me>
2024-10-27 19:43:21 +0100

    feat: add Game class to start the game

---
+++ src/games/Games.java
@@ -0,0 +1,13 @@
+package games;
+
+import swyth.cardgame.controller.GameController;
+import swyth.cardgame.model.Deck;
+import swyth.cardgame.view.View;
+
+public class Games {
+
+    public static void main(String[] args) {
+        GameController gameController = new GameController(new Deck(), new View());
+        gameController.run();
+    }
+}
